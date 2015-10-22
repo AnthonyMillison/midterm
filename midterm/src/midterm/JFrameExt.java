41c928e99d213fc81ec2f8a84b5460f3750f2240
AnthonyMillison <anthonybball52@gmail.com>
2015-10-22 15:24:09 -0700

    Not sure what to write for handler on line 210, code needs to be added on lines 148, 168, 210, 237. Project compiles but with limited functionality.

--- midterm/src/midterm/JFrameExt.java
+++ midterm/src/midterm/JFrameExt.java
@@ -24,7 +24,7 @@ import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class JFrameExt extends JFrame {
+public class JFrameExt extends JFrame implements ActionListener {
 
 	//shared variables
 	private JPanel targetBeanObject = null;
@@ -217,4 +217,72 @@ public class JFrameExt extends JFrame {
 	
 	}
 
-}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		 int i;
+	      String propName="", propValue="";
+	     
+	      //Determine the name, value and index of the property that changed.
+	      for (i=0; i<jtfPropValues.length;i++)
+	      {
+	        if (e.getSource()== jtfPropValues[i])
+	        {
+	         
+	          break;
+	        }
+	      }
+	 
+	      //Get the property name and the property value from the
+	      //JLabel and JTextfield corresponding to the i value.
+	    
+	 
+	 
+	      //Note that the property index for the above property in the pd
+	      //array is the same. So you can go to that index in pd array and
+	      //access its property type .
+	      Class propType=pd[i].getPropertyType();
+	      //Get the property type as a String
+	      String propTypeName = propType.getName( );
+	 
+	 
+	      //Create Object array for storing parameters
+	      Object [ ] params = new Object [1];
+	 
+	      //Depending upon property name, create correct parameter object.
+	      if (propTypeName.equals ("int") )
+	      {
+	        params [0] = new Integer (Integer.parseInt ( propValue ) );
+	      }
+	      else if (propTypeName.equals ("double"))
+	      {
+	        params [0] = new Double (Double.parseDouble ( propValue ));
+	      }
+	      else if (propTypeName.equals ("boolean"))
+	      {
+	        params [0] = new Boolean (propValue );
+	      }
+	      else if (propTypeName.equals ("java.lang.String" ) )
+	      {
+	        params [0] = propValue;
+	      }
+	      //Get the set method object.
+	      Method mset = pd[i].getWriteMethod();
+	      //Invoke set method and pass it target bean and parameters.
+	      try
+	      {
+	        mset.invoke (targetBeanObject, params);
+	      }
+	      catch (IllegalAccessException ex)
+	      {
+	      }
+	      catch (IllegalArgumentException ex)
+	      {
+	      }
+	      catch (InvocationTargetException ex)
+	      {
+	      }
+	    }
+		
+	}
+
+
